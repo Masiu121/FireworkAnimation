9ef470fb950a052bf71095a4f4732228b47d0f84
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 19:26:42 +0100

    Added FireworkAnimation

---
+++ core/src/com/oxology/firework/FireworkAnimation.java
@@ -0,0 +1,72 @@
+package com.oxology.firework;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class FireworkAnimation extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture firework;
+	List<Firework> fireworks;
+	static Texture smallFirework;
+
+	static List<Color> colors;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		firework = new Texture("Firework.png");
+		smallFirework = new Texture("FireworkSmall.png");
+		fireworks = new ArrayList<>();
+		colors = new ArrayList<>();
+		colors.add(Color.BLUE);
+		colors.add(Color.RED);
+		colors.add(Color.GREEN);
+		colors.add(Color.WHITE);
+	}
+
+	public static Color getRandomColor() {
+		int rand = (int) (random()*colors.size());
+
+		return colors.get(rand);
+	}
+
+	@Override
+	public void render () {
+		update();
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		for(Firework firework : fireworks) {
+			firework.draw(batch);
+		}
+		batch.end();
+	}
+
+	public void update() {
+		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+			spawnFirework();
+
+		for(Firework firework : fireworks) {
+			firework.update();
+		}
+	}
+
+	public void spawnFirework() {
+		fireworks.add(new Firework(firework));
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		firework.dispose();
+	}
+}
