7862a12c49b61ca04b1cdc2f2adb277a4c979279
Masiu121 <maks.kapolka@gmail.com>
2021-12-24 19:26:21 +0100

    Added Firework img

---
+++ core/src/com/oxology/firework/Firework.java
@@ -0,0 +1,62 @@
+package com.oxology.firework;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class Firework {
+    float opacity;
+    float fireworkXSpeed;
+    float fireworkYSpeed;
+    float x, y;
+    Texture texture;
+    List<SubFirework> subFireworks;
+    boolean subFireworksCreated;
+    Color color;
+
+    public Firework(Texture texture) {
+        this.texture = texture;
+        opacity = 1;
+        fireworkXSpeed = random()*5-2.5f;
+        fireworkYSpeed = 8;
+        x = Gdx.graphics.getWidth()/2 - this.texture.getWidth()/2;
+        subFireworks = new ArrayList<>();
+        subFireworksCreated = false;
+        color = FireworkAnimation.getRandomColor();
+    }
+
+    public void update() {
+        opacity = opacity-0.01f;
+        x = x+fireworkXSpeed;
+        y = y+fireworkYSpeed;
+        fireworkYSpeed -= 0.1f;
+
+        if(opacity < 0.1 && !subFireworksCreated) {
+            createSubFireworks();
+            subFireworksCreated = true;
+        }
+    }
+
+    public void createSubFireworks() {
+        for(int i = 0; i < 20; i++) {
+            subFireworks.add(new SubFirework(this.x, this.y, texture));
+        }
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.setColor(color.r, color.g, color.b, opacity);
+        batch.draw(texture, x, y);
+        if(!subFireworks.isEmpty()) {
+            for(SubFirework subFirework : subFireworks) {
+                subFirework.update();
+                subFirework.draw(batch);
+            }
+        }
+    }
+}
